6631a252635df1c281355c2b8bf9f4bee96b5751
Sidnei Dias <sidnei_jr11@hotmail.com>
2021-06-26 04:29:44 -0300

    Praticando os conceitos iniciais de Métodos

--- bytebank/src/bytebank/Conta.java
+++ bytebank/src/bytebank/Conta.java
@@ -5,4 +5,25 @@ public class Conta {
 	int    agencia;
 	int	   numero;
 	String titular;
+	
+	void depositar (double valor) {
+		this.saldo += valor;
+	}
+	
+	boolean sacar (double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		}
+		return false;
+	}
+	
+	boolean transferir (double valor, Conta destino) {
+		if (this.saldo >= valor) {
+			this.sacar(valor);
+			destino.depositar(valor);
+			return true;
+		}
+		return false;
+	}
 }
---
+++ bytebank/src/bytebank/TesteMetodo.java
@@ -0,0 +1,21 @@
+package bytebank;
+
+public class TesteMetodo { 
+	public static void main (String[] args) {
+		Conta contaDaSarah = new Conta();
+		Conta contaDoRaziel = new Conta();
+		
+		contaDaSarah.depositar(1000);
+		contaDoRaziel.depositar(300);
+		System.out.println("\nSaldo da Sarah: "  + contaDaSarah.saldo +
+						   "\nSaldo do Raziel: " + contaDoRaziel.saldo );
+		contaDaSarah.sacar(200);
+		contaDoRaziel.sacar(400);
+		System.out.println("\nSaldo da Sarah: "  + contaDaSarah.saldo +
+				   		   "\nSaldo do Raziel: " + contaDoRaziel.saldo );
+		contaDaSarah.transferir(1200, contaDoRaziel);
+		contaDoRaziel.transferir(250, contaDaSarah);
+		System.out.println("\nSaldo da Sarah: "  + contaDaSarah.saldo +
+		   		   		   "\nSaldo do Raziel: " + contaDoRaziel.saldo );		
+	}
+}
